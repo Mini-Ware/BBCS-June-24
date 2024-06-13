6d7a26ff473dd2807610266816f0f309b6d6bd4e
Mini-Ware <codauknow@gmail.com>
2024-06-13 12:09:41 +0800

    show dataset for rating

--- pages/rating.py
+++ pages/rating.py
@@ -3,13 +3,27 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.header("Community Rating")
+# rating to-do list
+# svc classification model
+# sentient analysis
+# filter out positive comments
 
-# input to get the url
+st.header("Community Rating")
+df = pd.read_csv("./dataset/comments.csv")
+
+st.subheader("Dataset")
+st.write(df.head())
+st.bar_chart(df.groupby(["category"]).size())
 
 # for testing
 # just use LLM to generate fake comments and ratings to train
+# "give me another 20 short website review comments about x accessibility problems, i want this to be csv format with just one column and don't put semicolons"
 
-# actual implementation
-# input to get their comments
-# input to get numeric rating 0-5
+st.text("")
+st.text("")
+st.text("")
+st.subheader("Give Comment")
+comment = st.text_input("Website improvements to reduce reliance on screen reader summariser?", value="there is a thing flying around the screen")
+
+
+
