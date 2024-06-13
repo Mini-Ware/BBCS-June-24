bc930d5d25f254e79a91b62ca282636d95862094
Javier Ng <ng.kaijie.javier@dhs.edu.sg>
2024-06-13 23:04:25 +0800

    add files

--- pages/recommend.py
+++ pages/recommend.py
@@ -1,12 +1,57 @@
 # import module
 import streamlit as st
 import pandas as pd
-import numpy as np
 
-st.header("Accessibility Recommendation")
-st.subheader("Choose the screenshot")
+import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import re
+from collections import Counter
+import requests
+from colorthief import ColorThief
 
-# button to recommend
-st.button("analyse accessibility issues (colour contrast)")
+image=st.file_uploader("upload your screenshot here")
 
-# simple VQA
+
+
+def get_dominant_colors():
+  thief = ColorThief(image)
+  palette = thief.get_palette(color_count=2)
+  return palette
+
+
+if image:
+  get_dominant_colors(image)
+  st.write(type(palette))
+# training the ai model :>
+  link = "./BBCS-June-24/dataset/colour contrast ratio - color_contrast_data.csv"
+
+# creating the dataframe
+  df = pd.read_csv(link)
+  df.head()
+
+
+# training the AI ?
+
+  X = df["Color1", "Color2", "Contrast Ratio"]
+  y = df["Binary Scoring"]
+
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+  scaler = StandardScaler()
+  X_train = scaler.fit_transform(X_train)
+  X_test = scaler.transform(X_test)
+
+  clf = LogisticRegression(random_state=2)
+
+  clf.fit(X_train, y_train)
+
+  predictions = clf.predict(X_test)
+
+  accuracy = accuracy_score(y_test, predictions)
+  st.write(predictions)
+  st.write("Accuracy", accuracy)
